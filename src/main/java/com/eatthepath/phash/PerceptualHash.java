b33d62b6135236a3c22c3d66204dbae304c02ba6
Jon Chambers <jon.chambers@gmail.com>
2018-01-04 00:07:29 -0500

    Cleaned house, added helper methods, and discarded example app.

--- src/main/java/com/eatthepath/phash/PerceptualHash.java
+++ src/main/java/com/eatthepath/phash/PerceptualHash.java
@@ -2,16 +2,37 @@ package com.eatthepath.phash;
 
 import org.jtransforms.dct.FloatDCT_2D;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
-public class PHashCalculator {
+public class PerceptualHash {
 
     private static final int SCALED_IMAGE_SIZE = 32;
-
     private static final FloatDCT_2D DCT = new FloatDCT_2D(SCALED_IMAGE_SIZE, SCALED_IMAGE_SIZE);
 
-    public static long calculateHash(final BufferedImage image) {
+    public static long getPerceptualHash(final File imageFile) throws IOException {
+        return getPerceptualHash(ImageIO.read(imageFile));
+    }
+
+    public static long getPerceptualHash(final ImageInputStream imageInputStream) throws IOException {
+        return getPerceptualHash(ImageIO.read(imageInputStream));
+    }
+
+    public static long getPerceptualHash(final InputStream imageInputStream) throws IOException {
+        return getPerceptualHash(ImageIO.read(imageInputStream));
+    }
+
+    public static long getPerceptualHash(final URL imageUrl) throws IOException {
+        return getPerceptualHash(ImageIO.read(imageUrl));
+    }
+
+    public static long getPerceptualHash(final Image image) {
         final BufferedImage scaledImage = new BufferedImage(SCALED_IMAGE_SIZE, SCALED_IMAGE_SIZE, BufferedImage.TYPE_BYTE_GRAY);
         {
             final Graphics2D graphics = scaledImage.createGraphics();
@@ -26,7 +47,7 @@ public class PHashCalculator {
         scaledImage.getData().getPixels(0, 0, SCALED_IMAGE_SIZE, SCALED_IMAGE_SIZE, dct);
         DCT.forward(dct, false);
 
-        double lowFrequencyDctAverage = -dct[0];
+        float lowFrequencyDctAverage = -dct[0];
 
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
