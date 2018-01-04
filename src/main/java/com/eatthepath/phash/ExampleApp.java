aa0cf9429ca2e013a217bdf10cb88dd1bf494907
Jon Chambers <jon.chambers@gmail.com>
2018-01-03 23:32:02 -0500

    Took a compositional approact to the DCT; I think it's still wrong.

--- src/main/java/com/eatthepath/phash/ExampleApp.java
+++ src/main/java/com/eatthepath/phash/ExampleApp.java
@@ -10,12 +10,13 @@ public class ExampleApp {
     public static void main(final String... args) throws IOException {
         final BufferedImage image = ImageIO.read(new File("/Users/jon/Desktop/test-image.png"));
         // final BufferedImage modifiedImage = ImageIO.read(new File("/Users/jon/Desktop/test-image-modified.png"));
-        final BufferedImage modifiedImage = ImageIO.read(new File("/Users/jon/Desktop/fax-machine.jpg"));
+        // final BufferedImage modifiedImage = ImageIO.read(new File("/Users/jon/Desktop/fax-machine.jpg"));
+        final BufferedImage modifiedImage = ImageIO.read(new File("/Users/jon/Desktop/ide.png"));
 
         final long hash = PHashCalculator.calculateHash(image);
-        final long modifiedHash = PHashCalculator.calculateHash(modifiedImage);
-
         System.out.format("0x%016X\n", hash);
+
+        final long modifiedHash = PHashCalculator.calculateHash(modifiedImage);
         System.out.format("0x%016X\n", modifiedHash);
 
         System.out.println(Long.bitCount(hash ^ modifiedHash));
--- src/main/java/com/eatthepath/phash/PHashCalculator.java
+++ src/main/java/com/eatthepath/phash/PHashCalculator.java
@@ -6,7 +6,18 @@ import java.awt.image.BufferedImage;
 public class PHashCalculator {
 
     private static final int SCALED_IMAGE_SIZE = 32;
-    private static final int DCT_AVERAGE_SIZE = SCALED_IMAGE_SIZE / 4;
+
+    private static final double[][] DCT_COEFFICIENTS;
+
+    static {
+        DCT_COEFFICIENTS = new double[8][SCALED_IMAGE_SIZE];
+
+        for (int k = 0; k < 8; k++) {
+            for (int n = 0; n < SCALED_IMAGE_SIZE; n++) {
+                DCT_COEFFICIENTS[k][n] = Math.cos((Math.PI / SCALED_IMAGE_SIZE) * k * (n + 0.5));
+            }
+        }
+    }
 
     public static long calculateHash(final BufferedImage image) {
         final BufferedImage scaledImage = new BufferedImage(SCALED_IMAGE_SIZE, SCALED_IMAGE_SIZE, BufferedImage.TYPE_BYTE_GRAY);
@@ -25,26 +36,17 @@ public class PHashCalculator {
             scaledImage.getData().getPixels(0, 0, SCALED_IMAGE_SIZE, SCALED_IMAGE_SIZE, pixels);
 
             for (int y = 0; y < SCALED_IMAGE_SIZE; y++) {
-                for (int x = 0; x < SCALED_IMAGE_SIZE; x++) {
-                    System.out.format("%d,", (int) pixels[x + (y * SCALED_IMAGE_SIZE)]);
+                for (int k = 0; k < 8; k++) {
+                    for (int x = 0; x < SCALED_IMAGE_SIZE; x++) {
+                        dct[k + (y * SCALED_IMAGE_SIZE)] += pixels[x + (y * SCALED_IMAGE_SIZE)] * DCT_COEFFICIENTS[k][x];
+                    }
                 }
-
-                System.out.format(";");
             }
 
-            System.out.format("\n");
-
-            final double PI_N = Math.PI / SCALED_IMAGE_SIZE;
-
-            for (int j = 0; j < SCALED_IMAGE_SIZE; j++) {
-                for (int k = 0; k < SCALED_IMAGE_SIZE; k++) {
-                    for (int x = 0; x < SCALED_IMAGE_SIZE; x++) {
-                        double xComponent = Math.cos(PI_N * j * (x + 0.5));
-
-                        for (int y = 0; y < SCALED_IMAGE_SIZE; y++) {
-                            dct[j + (k * SCALED_IMAGE_SIZE)] +=
-                                    pixels[x + (y * SCALED_IMAGE_SIZE)] * xComponent * Math.cos(PI_N * k * (y + 0.5));
-                        }
+            for (int x = 0; x < 8; x++) {
+                for (int k = 0; k < 8; k++) {
+                    for (int y = 0; y < SCALED_IMAGE_SIZE; y++) {
+                        dct[x + (k * SCALED_IMAGE_SIZE)] += dct[x + (y * SCALED_IMAGE_SIZE)] * DCT_COEFFICIENTS[k][y];
                     }
                 }
             }
@@ -52,27 +54,25 @@ public class PHashCalculator {
 
         double lowFrequencyDctAverage = -dct[0];
 
-        for (int x = 0; x < DCT_AVERAGE_SIZE; x++) {
-            for (int y = 0; y < DCT_AVERAGE_SIZE; y++) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                System.out.println(dct[x + (y * SCALED_IMAGE_SIZE)]);
                 lowFrequencyDctAverage += dct[x + (y * SCALED_IMAGE_SIZE)];
             }
         }
 
-        lowFrequencyDctAverage /= (DCT_AVERAGE_SIZE * DCT_AVERAGE_SIZE);
+        lowFrequencyDctAverage /= 64;
 
         long hash = 0;
 
-        for (int y = 0; y < 8; y++) {
-            byte b = 0;
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                hash <<= 1;
 
-            for (int x = 0; x < 8; x++) {
-                if (dct[x + (y * 8)] > lowFrequencyDctAverage) {
-                    b |= (1 << x);
+                if (dct[x + (y * SCALED_IMAGE_SIZE)] > lowFrequencyDctAverage) {
+                    hash |= 1;
                 }
             }
-
-            hash <<= 8;
-            hash |= (b & 0xff);
         }
 
         return hash;
